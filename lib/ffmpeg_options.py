1e1688b2a098c5bcdc0bf2ad727a28ffda3709d1
ad-mitium <99030354+ad-mitium@users.noreply.github.com>
2023-01-25 08:48:03 -0500

    Tweak special options

--- lib/ffmpeg_options.py
+++ lib/ffmpeg_options.py
@@ -25,7 +25,7 @@ copy_files={
 special={       # Default ffmpeg presets were adding distracting amounts of pixelation during transcoding, changed to yuv420p from nv12
     'special_sub': '-map 0:v -map 0:a -map 0:{stream}? -c:v {gpu_codec} -rc vbr_latency -qmin 26 -pix_fmt yuv420p -b:v 0K -vtag hvc1 -c:a copy -c:s mov_text -metadata:s:s:0 language=en',
     'special_copy': '-map 0:v -map 0:a -map 0:{stream}? -c:v {gpu_codec} -rc vbr_latency -qmin 26 -pix_fmt yuv420p -b:v 0K -vtag hvc1 -c:a copy -c:s copy',
-    'special_trans': '-map 0:v -map 0:a -map 0:{stream}? -c:v {gpu_codec} -rc vbr_latency -qmin 26 -pix_fmt yuv420p -b:v 0K -vtag hvc1 -c:a copy '
+    'special_trans': '-map 0:v -map 0:a -c:v {gpu_codec} -rc vbr_latency -qmin 26 -pix_fmt yuv420p -b:v 0K -vtag hvc1 -c:a copy '
 }
 
 
